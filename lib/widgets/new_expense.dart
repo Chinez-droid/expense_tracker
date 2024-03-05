494c1e81b07c4a507208736cde7654b5dbc5b1c4
Chinez-droid <chineduezeozue@gmail.com>
2024-03-05 18:52:40 +0100

    Push 1

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -13,11 +13,28 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   // better alternative to storing all the inputs for textField
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
+
+  // method for selecting date picker
+  void _presentDatePicker() {
+    // inital date parameter
+    final now = DateTime.now();
+
+    // first date paremeter
+    final firstDate = DateTime(now.year - 20, now.month, now.day);
+
+    showDatePicker(
+        context: context,
+        initialDate: now,
+        firstDate: firstDate,
+        lastDate: now);
+  }
 
   // deletes the controller when the widget ain't needed anymore
   @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
 
@@ -37,11 +54,48 @@ class _NewExpenseState extends State<NewExpense> {
           ),
           Row(
             children: [
+              Expanded(
+                child: TextField(
+                  controller: _amountController,
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    prefixText: '\$',
+                    label: Text('Amount'),
+                  ),
+                ),
+              ),
+              const SizedBox(width: 16),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text('Selected Date'),
+                    IconButton(
+                      onPressed: _presentDatePicker,
+                      icon: const Icon(
+                        Icons.calendar_month,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancel'),
+              ),
               ElevatedButton(
                 onPressed: () {
                   print(_titleController.text);
+                  print(_amountController.text);
                 },
-                child: Text('Save Expense'),
+                child: const Text('Save Expense'),
               ),
             ],
           )
